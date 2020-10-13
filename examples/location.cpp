1ff2371f55b9215d03fc63867ef63c36445cc5a6
Arthur Komatsu <arthur.komatsu.aroeira@gmail.com>
2020-10-13 00:27:21 -0300

    Adding first example
    Task:
    
    O que foi feito:
    
    Signed-off-by: Arthur Komatsu <arthur.komatsu.aroeira@gmail.com>

---
+++ examples/location.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <esp_wifi_location.h>
+
+esp_wifi_location location;
+#define LOCATION_GOOGLE_KEY "INSERT YOUR GOOGLE API KEY HERE"
+
+void setup()
+{
+        Serial.begin(115200);
+
+        WiFi.begin("INSERT_YOUR_SSID_HERE", "INSERT_YOUR_PASS_HERE");
+        while (WiFi.isConnected() == false) {
+                delay(500);
+                Serial.print(".");
+        }
+        Serial.println("Connected, getting location...");
+        double lat, lng, acc;
+        location.setGoogleKey(LOCATION_GOOGLE_KEY);
+        if (location.getLocation(lat, lng, acc) == true)
+                Serial.printf("Location success: %.6f, %.6f (%.2fm)\n", lat, lng, acc);
+        else
+                Serial.println("Location failed");
+}
+
+void loop()
+{
+
+}
